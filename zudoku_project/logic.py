c7b1e614c91b6a66028892531c286a80c44aa144
unknown <enmanuelpuglisi@gmail.com>
2024-05-07 19:51:07 +0200

    zudoku 1.0

--- zudoku_project/logic.py
+++ zudoku_project/logic.py
@@ -1,4 +1,4 @@
-from random import randrange, shuffle, choice
+from random import randrange, shuffle, choice, sample
 from time import sleep
 
 board = [[" "," "," "," "," "," "," "," "," "],
@@ -11,29 +11,28 @@ board = [[" "," "," "," "," "," "," "," "," "],
          [" "," "," "," "," "," "," "," "," "],
          [" "," "," "," "," "," "," "," "," "]]
 
-
-#Temporal display on terminal
 def boarddisplay():
     print(f'''
-\033[1m+---+---+---+---+---+---+---+---+---+\033[0m
-\033[1m|\033[0m {board[0][0]} | {board[0][1]} | {board[0][2]} \033[1m|\033[0m {board[0][3]} | {board[0][4]} | {board[0][5]} \033[1m|\033[0m {board[0][6]} | {board[0][7]} | {board[0][8]} \033[1m|\033[0m
-\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---+\033[0m
-\033[1m|\033[0m {board[1][0]} | {board[1][1]} | {board[1][2]} \033[1m|\033[0m {board[1][3]} | {board[1][4]} | {board[1][5]} \033[1m|\033[0m {board[1][6]} | {board[1][7]} | {board[1][8]} \033[1m|\033[0m
-\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---+\033[0m
-\033[1m|\033[0m {board[2][0]} | {board[2][1]} | {board[2][2]} \033[1m|\033[0m {board[2][3]} | {board[2][4]} | {board[2][5]} \033[1m|\033[0m {board[2][6]} | {board[2][7]} | {board[2][8]} \033[1m|\033[0m
-\033[1m+---+---+---+---+---+---+---+---+---+\033[0m
-\033[1m|\033[0m {board[3][0]} | {board[3][1]} | {board[3][2]} \033[1m|\033[0m {board[3][3]} | {board[3][4]} | {board[3][5]} \033[1m|\033[0m {board[3][6]} | {board[3][7]} | {board[3][8]} \033[1m|\033[0m
-\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---+\033[0m
-\033[1m|\033[0m {board[4][0]} | {board[4][1]} | {board[4][2]} \033[1m|\033[0m {board[4][3]} | {board[4][4]} | {board[4][5]} \033[1m|\033[0m {board[4][6]} | {board[4][7]} | {board[4][8]} \033[1m|\033[0m
-\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---+\033[0m
-\033[1m|\033[0m {board[5][0]} | {board[5][1]} | {board[5][2]} \033[1m|\033[0m {board[5][3]} | {board[5][4]} | {board[5][5]} \033[1m|\033[0m {board[5][6]} | {board[5][7]} | {board[5][8]} \033[1m|\033[0m
-\033[1m+---+---+---+---+---+---+---+---+---+\033[0m
-\033[1m|\033[0m {board[6][0]} | {board[6][1]} | {board[6][2]} \033[1m|\033[0m {board[6][3]} | {board[6][4]} | {board[6][5]} \033[1m|\033[0m {board[6][6]} | {board[6][7]} | {board[6][8]} \033[1m|\033[0m
-\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---+\033[0m
-\033[1m|\033[0m {board[7][0]} | {board[7][1]} | {board[7][2]} \033[1m|\033[0m {board[7][3]} | {board[7][4]} | {board[7][5]} \033[1m|\033[0m {board[7][6]} | {board[7][7]} | {board[7][8]} \033[1m|\033[0m
-\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---+\033[0m
-\033[1m|\033[0m {board[8][0]} | {board[8][1]} | {board[8][2]} \033[1m|\033[0m {board[8][3]} | {board[8][4]} | {board[8][5]} \033[1m|\033[0m {board[8][6]} | {board[8][7]} | {board[8][8]} \033[1m|\033[0m
-\033[1m+---+---+---+---+---+---+---+---+---+\033[0m
+     1   2   3   4   5   6   7   8   9 
+   \033[1m+---+---+---+---+---+---+---+---+---+\033[0m
+A  \033[1m|\033[0m {board[0][0]} | {board[0][1]} | {board[0][2]} \033[1m|\033[0m {board[0][3]} | {board[0][4]} | {board[0][5]} \033[1m|\033[0m {board[0][6]} | {board[0][7]} | {board[0][8]} \033[1m|\033[0m
+   \033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---+\033[0m
+B  \033[1m|\033[0m {board[1][0]} | {board[1][1]} | {board[1][2]} \033[1m|\033[0m {board[1][3]} | {board[1][4]} | {board[1][5]} \033[1m|\033[0m {board[1][6]} | {board[1][7]} | {board[1][8]} \033[1m|\033[0m
+   \033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---+\033[0m
+C  \033[1m|\033[0m {board[2][0]} | {board[2][1]} | {board[2][2]} \033[1m|\033[0m {board[2][3]} | {board[2][4]} | {board[2][5]} \033[1m|\033[0m {board[2][6]} | {board[2][7]} | {board[2][8]} \033[1m|\033[0m
+   \033[1m+---+---+---+---+---+---+---+---+---+\033[0m
+D  \033[1m|\033[0m {board[3][0]} | {board[3][1]} | {board[3][2]} \033[1m|\033[0m {board[3][3]} | {board[3][4]} | {board[3][5]} \033[1m|\033[0m {board[3][6]} | {board[3][7]} | {board[3][8]} \033[1m|\033[0m
+   \033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---+\033[0m
+E  \033[1m|\033[0m {board[4][0]} | {board[4][1]} | {board[4][2]} \033[1m|\033[0m {board[4][3]} | {board[4][4]} | {board[4][5]} \033[1m|\033[0m {board[4][6]} | {board[4][7]} | {board[4][8]} \033[1m|\033[0m
+   \033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---+\033[0m
+F  \033[1m|\033[0m {board[5][0]} | {board[5][1]} | {board[5][2]} \033[1m|\033[0m {board[5][3]} | {board[5][4]} | {board[5][5]} \033[1m|\033[0m {board[5][6]} | {board[5][7]} | {board[5][8]} \033[1m|\033[0m
+   \033[1m+---+---+---+---+---+---+---+---+---+\033[0m
+G  \033[1m|\033[0m {board[6][0]} | {board[6][1]} | {board[6][2]} \033[1m|\033[0m {board[6][3]} | {board[6][4]} | {board[6][5]} \033[1m|\033[0m {board[6][6]} | {board[6][7]} | {board[6][8]} \033[1m|\033[0m
+   \033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---+\033[0m
+H  \033[1m|\033[0m {board[7][0]} | {board[7][1]} | {board[7][2]} \033[1m|\033[0m {board[7][3]} | {board[7][4]} | {board[7][5]} \033[1m|\033[0m {board[7][6]} | {board[7][7]} | {board[7][8]} \033[1m|\033[0m
+   \033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---\033[1m+\033[0m---+---+---+\033[0m
+I  \033[1m|\033[0m {board[8][0]} | {board[8][1]} | {board[8][2]} \033[1m|\033[0m {board[8][3]} | {board[8][4]} | {board[8][5]} \033[1m|\033[0m {board[8][6]} | {board[8][7]} | {board[8][8]} \033[1m|\033[0m
+   \033[1m+---+---+---+---+---+---+---+---+---+\033[0m
 ''')
 
 def check(boardd):
@@ -49,6 +48,7 @@ def check(boardd):
                     if group.count(number) > 1:
                         return False
     return True
+
 def randomgenerator():
     global board
     hisd= ""
@@ -74,7 +74,7 @@ def randomgenerator():
             try:
                 for el in blacklist[str(t)]:
                     for e in el:
-                        # print(e, "E", t, "TURN")
+
                         indexes= indexes.replace(e,"")
             except:
                     pass
@@ -118,6 +118,125 @@ def randomgenerator():
                 
             else:
                 history.append(f'{r}:{i}:{t}')
-                t+=1
-                
+                t+=1     
+
+def difficulty():
+    global board
+    dif = input("Choose a difficulty please (Easy, Medium, Hard): ").capitalize()
+    while dif not in ("Easy", "Medium", "Hard", 'Test'):
+        dif = input("Please choose a valid difficulty (Easy, Medium, Hard): ").capitalize()
+
+    if dif == 'Test':
+        empty_cells = 1
+    elif dif == 'Easy':
+        empty_cells = 40
+    elif dif == 'Medium':
+        empty_cells = 50
+    else:  # 'Hard'
+        empty_cells = 60
+        
+
+    for __ in range(empty_cells):
+        row, col = sample(range(9), 2)
+        while board[row][col] == " ":
+            row, col = sample(range(9), 2)
+        board[row][col] = " "
+def picking():
+    global board
+    global lives
+
+    loc = input("Choose a cell (in a pattern like C3, H1): ")
+
+    def cordchecker(loc):
+        global board
+        while True:
             
+            if loc[0] not in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'] or loc[1] not in ['1', '2', '3', '4', '5', '6', '7', '8', '9'] or len(loc) != 2:
+                loc = input("Please insert a valid pattern (C3, H1): ")
+                continue
+            x = ord(loc[0].upper()) - ord('A')
+            y = int(loc[1]) - 1
+            print(x,y)
+            boarddisplay()
+            if board[x][y] == " ":
+                return x, y
+            else:
+                loc = input("Please choose an empty cell: ")
+
+    def numberchoosing(x, y):
+        global board
+        number = input("Please insert a number to put in cell (to change cell, insert new coord): ")
+        if len(number) == 1 and number.isdigit():
+            board[x][y] = number
+        elif number[0] in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'] and number[1] in ['1', '2', '3', '4', '5', '6', '7', '8', '9'] and len(number) == 2:
+            x,y = cordchecker(number)
+            numberchoosing(x,y)
+        # else:
+        #     print("Invalid input! You must enter a single-digit number.")
+
+
+    def checker(x,y):
+        print("im here!")
+        
+        global board
+        global lives
+        while not check(board):
+            print("Wrong number! You lost a life. You have", lives, "lives left.")
+            board[x][y] = " "
+            lives -= 1
+            if lives == 0:
+                print("Game over!")
+                return False
+            loc = input("Please insert a new number or change coord: ")
+            x, y = cordchecker(loc)
+            print(x,y, 'IN checker')
+            numberchoosing(x, y)
+
+    x, y = cordchecker(loc)
+    
+    numberchoosing(x, y)
+    checker(x,y)
+   
+def winner_check():
+    global board
+    for row in range(9):
+        for number in range(9):
+            if board[row][number] == " ":
+                return True
+    return False
+    
+def try_again():
+    global board
+    board = [[" "," "," "," "," "," "," "," "," "],
+         [" "," "," "," "," "," "," "," "," "],
+         [" "," "," "," "," "," "," "," "," "],
+         [" "," "," "," "," "," "," "," "," "],
+         [" "," "," "," "," "," "," "," "," "],
+         [" "," "," "," "," "," "," "," "," "],
+         [" "," "," "," "," "," "," "," "," "],
+         [" "," "," "," "," "," "," "," "," "],
+         [" "," "," "," "," "," "," "," "," "]]
+    sudoku()
+                
+def sudoku():
+    global lives
+    print("Welcome to Sudoku!")
+    lives = 5
+    sleep(1)
+    randomgenerator()
+    difficulty()
+    boarddisplay()
+    while winner_check() and lives != 0:
+        picking()
+        boarddisplay()
+        
+    if lives == 0:
+        print("Game over!, Good try!")
+        try_again()
+    else:
+        print("Congrats!, you won!")
+        try_again()
+
+sudoku()
+        
+        
